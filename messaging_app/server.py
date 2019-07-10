6bd278a16ffef35209002c3d7022d48c662f62a1
ishan <ishan747@uab.edu>
2019-07-10 14:50:59 -0500

    update

--- messaging_app/server.py
+++ messaging_app/server.py
@@ -2,7 +2,7 @@ import socket
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # serv.bind is the ip the server creates and client connects to.
-serv.bind(('0.0.0.0', 8080))
+serv.bind(('ec2-54-80-18-12.compute-1.amazonaws.com', 8080))
 serv.listen(5)
 while True:
     conn, addr = serv.accept()
