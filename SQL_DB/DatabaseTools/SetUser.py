52bd5fa1b41dc7bdd87767bc2c4f79de7088b257
Danskiboo <danielglowe95@gmail.com>
2019-07-11 20:24:40 -0500

    fixted imports to setters for user's names

---
+++ SQL_DB/DatabaseTools/SetUser.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# script to set a new username in database for user
+import sqlite3 as lite
+from .CheckEmail import checkEmail
+
+from .Variables import *
+
+# connect to database with sqlite
+db = "UserInfo.db"
+con = lite.connect(db)
+table = "Users"
+
+def setUser(user_email, username):
+    # table = "Users"
+
+    with con:
+        cur = con.cursor()
+
+        if checkEmail(table, user_email)[0]:
+
+            # Insert pin to 
+            cur.execute("UPDATE {} Set UserName = '{}' WHERE Email = '{}';".format(table, username, user_email))
+            return True
+        else:
+            return False
+
+
+# print(setUser("i@gmail.com","NewUser"))
