12c5d7f3b5c9d448ecb546f02cb827150ab0c437
Danskiboo <danielglowe95@gmail.com>
2019-07-11 22:26:11 -0500

    Handle several db interactions with JSON files  - Restructure of JSON file proposed

--- SQL_DB/HandleJSON.py
+++ SQL_DB/HandleJSON.py
@@ -6,17 +6,23 @@ import sqlite3
 from DatabaseTools.SetFirst import setFirst
 from DatabaseTools.SetLast import setLast
 from DatabaseTools.SetUser import setUser
+from DatabaseTools.DeleteUser import deleteUser
+from DatabaseTools.CreateUser import createUser
+
+from DatabaseTools.DeleteAllFriends import deleteAllFriends
+from DatabaseTools.DeleteFriend import deleteFriend
+from DatabaseTools.AddFriend import addFriend
 
 # from DatabaseTools import SetUser
 
 jobj = {
-	"Handle": '8',
+	"Handle": '9',
 	
 	"UserInfo" : {
 	  "Id": "",
 	  "FirstName": "Daniel",	
-	  "LastName": "ManLost",
-	  "UserName": "Danskiboy",
+	  "LastName": "Lowe",
+	  "UserName": "toesareroses",
 	  "Email": "d@gmail.com",
 	  "Password": "lolol",
 	  "PIN": "1234",
@@ -24,6 +30,10 @@ jobj = {
 	  "Friends": "",
 	  "Connection": ""
 	},
+
+	"FriendEmail" : "s@gmail.com",
+
+	"FriendEmailList" : ["c@gmail.com", "m@gmail.com"],
 
 	"MadeItMessage" : "ImDead"
 	}
@@ -37,56 +47,60 @@ def handleJSON(jObject):
 	
 	userInfo = y["UserInfo"]
 	message = y["MadeItMessage"]
+	friendEmail = y["FriendEmail"]
+	friendEmailList = y["FriendEmailList"]
 
 	if handle == 0:
 		# create a new user
-		print("# create a new user")
-		# TODO
+		# print("# create a new user")
+		createUser(userInfo["FirstName"],userInfo["LastName"],userInfo["UserName"],\
+			userInfo["Password"],userInfo["Email"])
 
 	elif handle == 1:
 		# User 'MadeIt' - Contact friends
 		print("# User 'MadeIt' - Contact friends")
 		# TODO
 
+
 	elif handle == 2:
-		# Add Freinds to user
-		print("# Add Freinds to user")
-		# TODO
+		# Add Freind to user
+		addFriend(userInfo["Email"],friendEmail)
 	
 	elif handle == 3:
 		# Remove friend from users list
-		print("# Remove friend from user's list")
-		# TODO
+		deleteFriend(userInfo["Email"],friendEmail)
 
 	elif handle == 4:
-		# User 'MadeIt' - Contact friends
-		print("# User 'MadeIt' - Contact friends")
-		# TODO
+		# Delete All Friends from User's Frineds list
+		deleteAllFriends(userInfo["Email"])
 
 	elif handle == 5:
 		# Delete a User's account
-		print("# Delete a User's account")
+		deleteUser(userInfo["Email"])
 
 
 	elif handle == 6:
 		# Set User's First Name
-		# print("# Set User's First Name")
-		print(userInfo["Email"], userInfo["FirstName"])
 		setFirst(userInfo["Email"], userInfo["FirstName"])
 		
 
 	elif handle == 7:
 		# Set User's Last Name
-		# print("# Set User's Last Name")
-		print(userInfo["Email"], userInfo["LastName"])
 		setLast(userInfo["Email"], userInfo["LastName"])
 
 
 	elif handle == 8:
 		# Set user's UserName
-		print("# Set user's UserName")
-
-		print(userInfo["Email"], userInfo["UserName"])
 		setUser(userInfo["Email"], userInfo["UserName"])
+
+	elif handle == 9:
+		# Add List of Friends to user
+		for friend in friendEmailList:
+			addFriend(userInfo["Email"],friend)
+
+	elif handle == 10:
+		# Delete List of Friends to User
+		for friend in friendEmailList:
+			deleteFriend(userInfo["Email"],friend)
 
 handleJSON(jobj)
