4326be6376086ef4036d75b2a4c220c9a4a3829e
Danskiboo <danielglowe95@gmail.com>
2019-07-19 23:38:14 -0500

    Getters for information needed with made it message - code not yet added

---
+++ SQL_DB/DatabaseTools/GetFriendsInfo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# returns a list of tuples containing the friends names and connection info
+
+# The returned friends list can be iterated over and all retJSONs passed through connectToClient function
+
---
+++ SQL_DB/DatabaseTools/GetUserInfo.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+
+# returns the information needed by HandleJSON to pack and send the retJSON 
+
